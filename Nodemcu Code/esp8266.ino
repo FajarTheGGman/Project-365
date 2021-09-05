c08cd25aabe2e4398d464a5f6ee82cbfc9f23e57
Fajar Firdaus <35714197+FajarTheGGman@users.noreply.github.com>
2021-09-05 14:52:55 +0700

    Update esp8266.ino

--- Nodemcu Code/esp8266.ino
+++ Nodemcu Code/esp8266.ino
@@ -9,8 +9,8 @@ SoftwareSerial mainMachine(5, 6);
 ESP8266WebServer server(80);
 HTTPClient http;
 
-String cloudServer = "http://192.168.1.9:5000/";
-String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IkZhamFyIiwicGFzc3dvcmQiOiIkMmIkMTAkUmt5Y041ZnpFSG9mVGpYN1NJTTRXdUxHZ2hkUnFMcTFZT0NkdHVKbzRtdnhjeUg1MmUvQUsiLCJpYXQiOjE2MzAwMzA5NDl9.ZATeLVH-eatbmducQ5qLpFYOHt_372ThVebOsEjeFLc";
+String cloudServer = "http://yourserver.com/";
+String token = "yourtoken";
 String key = "Important";
 
 String get_relay = cloudServer + "board/relay/getall?token=" + token + "&secret=" + key;
@@ -18,110 +18,68 @@ String get_spesific_relay = cloudServer + "board/relay?token=" + token + "&secre
 String send_location = cloudServer + "board/location?token=" + token + "&secret=" + key + "&location="; 
 String get_location = "http://ip-api/json";
 
+// Initialize your ssid
+char* ssid = "yourssdname";
+char* pwd = "yourpassword";
 
-char* ssid = "ServerIOT";
-char* pwd = "fajarfirdaus";
+// [Offline Mode] define a relays pin
+#define relay1 D0 // or another pin, it's up to you
+#define relay2 D1 // or another pin, it's up to you
+#define relay3 D2 // or another pin, it's up to you
+#define relay4 D3 // or another pin, it's up to you
 
-
-void relay(){
-    String get_pin = server.arg("pin");
-    int pin = get_pin.toInt();
-    
-    if(server.arg("volt") == "HIGH"){
-      digitalWrite(pin, LOW);
-      server.send(200, "text/plain", "{ pin " + get_pin + ": ON } ");
-    }else if(server.arg("volt") == "LOW"){
-      digitalWrite(pin, HIGH);
-      server.send(200, "text/plain", "{ pin " + get_pin + ": OFF } ");
-    }else{
-      server.send(200, "text/plain", "{ error: 'No Arguments' }");
-    }
+// [Offline Mode] relay function [ ON ]
+void relayone(){
+    digitalWrite(relay1, LOW);
+    server.send(200, "text/plain", "{ Relay1: ON }");
 }
 
-void readSensor(){
-  String get_pin = server.arg("pin");
-  int pin = get_pin.toInt();
-
-  if(server.arg("type") == "analog"){
-    int data_sensor = analogRead(pin);
-    String str = String(data_sensor);
-    server.send(200, "text/plain", str);
-  }else if(server.arg("type") == "digital"){
-    int data_sensor = digitalRead(pin);
-    String str = String(data_sensor);
-    server.send(200, "text/plain", str);
-  }else{
-    server.send(200, "text/plain","{ Error: 'No Arguments' }");
-  }
+void relaytwo(){
+    digitalWrite(relay2, LOW);
+    server.send(200, "text/plain", "{ Relay2: ON }");
 }
 
-
-void relaysatu(){
-   server.send(200, "text/plain", "{'Relay1': 'Menyala'}");
-   digitalWrite(D0, LOW);
+void relaythree(){
+    digitalWrite(relay3, LOW);
+    server.send(200, "text/plain", "{ Relay3: ON }");
 }
 
-void relaydua(){
-  server.send(200, "text/plain", "{'Relay2': 'Menyala'}");
-  digitalWrite(D1, LOW);
+void relayfour(){
+    digitalWrite(relay4, LOW);
+    server.send(200, "text/plain", "{ Relay4: ON }");
 }
 
-void relaytiga(){
-  server.send(200, "text/plain", "{'Relay3': 'Menyala'}");
-  digitalWrite(D2, LOW);
+// [Offline Mode] relay function [ OFF ]
+void relayonedie(){
+    digitalWrite(relay1, HIGH);
+    server.send(200, "text/plain", "{ Relay1: OFF }");
 }
 
-void relayempat(){
-  server.send(200, "text/plain", "{'Relay4': 'Menyala'}");
-  digitalWrite(D3, LOW);
+void relaytwodie(){
+    digitalWrite(relay2, HIGH);
+    server.send(200, "text/plain", "{ Relay2: OFF }");
 }
 
-// Relay Mode Mati
-
-void relaysatumati(){
-   server.send(200, "text/plain", "{'Relay1': 'Mati'}");
-   digitalWrite(D0, HIGH);
+void relaythreedie(){
+    digitalWrite(relay3, HIGH);
+    server.send(200, "text/plain", "{ Relay3: OFF }");
 }
 
-void relayduamati(){
-  server.send(200, "text/plain", "{'Relay2': 'Mati'}");
-  digitalWrite(D1, HIGH);
+void relayfourdie(){
+    digitalWrite(relay4, HIGH);
+    server.send(200, "text/plain", "{ Relay4: OFF }");
 }
 
-void relaytigamati(){
-  server.send(200, "text/plain", "{'Relay3': 'Mati'}");
-  digitalWrite(D2, HIGH);
-}
-
-void relayempatmati(){
-  server.send(200, "text/plain", "{'Relay4': 'Mati'}");
-  digitalWrite(D3, HIGH);
-}
-
-void generate(int pin, bool volt, bool type){
-  if(type){
-    if(volt){
-      digitalWrite(pin, HIGH); 
-    }else{
-      digitalWrite(pin, LOW); 
-    }
- }else{
-     if(volt){
-      analogWrite(pin, HIGH); 
-    }else{
-      analogWrite(pin, LOW); 
-    }
-  }
-}
-
-void setup() {
+void setup(void) {
   Serial.begin(115200);
   mainMachine.begin(9600);
   WiFi.begin(ssid, pwd);
-  pinMode(D0, OUTPUT);
-  pinMode(D1, OUTPUT);
-  pinMode(D2, OUTPUT);
-  pinMode(D3, OUTPUT);
+    
+  // [Offline Mode] Initialize pin
+  pinMode(relay1, OUTPUT);
+  pinMode(relay2, OUTPUT);
+  pinMode(relay3, OUTPUT);
+  pinMode(relay4, OUTPUT);
 
  if(WiFi.status() != WL_CONNECTED){
   delay(100);
@@ -145,59 +103,22 @@ server.on("/", [](){
   server.send(200, "text/plain", "{ Wellcome: 'Project - 365%' }");
 });
 
-server.on("/relay", relay);
-server.on("/read", readSensor);
+// [Offline Mode] routing url
+server.on("/relay1", relayone);
+server.on("/relay2", relaytwo);
+server.on("/relay3", relaythree);
+server.on("/relay4", relayfour);
 
-server.on("/relay1", relaysatu);
-server.on("/relay2", relaydua);
-server.on("/relay3", relaytiga);
-server.on("/relay4", relayempat);
-
-server.on("/relay1mati", relaysatumati);
-server.on("/relay2mati", relayduamati);
-server.on("/relay3mati", relaytigamati);
-server.on("/relay4mati", relayempatmati);
+server.on("/relay1die", relayonedie);
+server.on("/relay2die", relaytwodie);
+server.on("/relay3die", relaythreedie);
+server.on("/relay4die", relayfourdie);
 
 
 server.begin();
-
-// Get board location 
-String coordinates;
-http.begin(get_location);
-
-if(http.GET() == 200){
-  coordinates = http.getString();
-}else{
-  Serial.println("[!] Error GET location data");
-}
-
-http.end();
-
-http.begin(send_location + coordinates);
-
-if(http.GET() == 200){
-  Serial.println(http.getString());
-}else{
-  Serial.println("[!] Error Sending data");
-}
-
-http.end();
-
 }
 
 
 void loop(void) {
   server.handleClient();
-
-  http.begin(get_relay);
-  if(http.GET() == 200){
-    Serial.println(http.getString());
-  }else if(http.GET() == 404){
-    Serial.println("relay/getall -> 404");
-  }else{
-    Serial.println("[!] Network Unreachable");
-  }
-  http.end();
-  
-  delay(3000);
 }
