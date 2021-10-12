c6c91683746f95e3094e76fdbeef5f75809bbf90
FajarTheGGman <newstrooper731@gmail.com>
2021-10-12 14:48:33 +0700

    fix conflict

--- src/index.js
+++ src/index.js
@@ -2,18 +2,11 @@ const app = require('./app');
 const chalk = require('chalk');
 const jsome = require('jsome');
 const color = require('colors');
-<<<<<<< HEAD
-//const box = require('boxen')
-//const loading = require('ora')
 const open = require('open')
-
-//console.log(color.rainbow(box("Smarthome 2.0", { padding: 1, borderStyle: 'double' }) + '\n'))
-=======
 const say = require('yosay')
 const open = require('open')
 
 console.log(say('Project 365%'))
->>>>>>> 09228086f61e8676bf6daa2943ba9f718038d4e2
 
 jsome({
     'Coder': 'Fajar Firdaus',
